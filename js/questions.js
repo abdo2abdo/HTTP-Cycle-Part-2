ae303f6a0f8122cc66629f42c9f69eda7fe4c174
AbdulRahman Awad <bw3169034@gmail.com>
2022-07-07 13:29:28 +0200

    Http_Cycle

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "when user write the domain sends the request to?",
+    answer: "Domain",
+    options: [
+      "anyone",
+      "Client",
+      "developer",
+      "Domain"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is responsible to see IP-Address?",
+    answer: "Domain Name Server",
+    options: [
+      "Domain Name Server",
+      "Domain Name web",
+      "Domain Name site",
+      "Domain Name Silent"
+    ]
+  },
+    {
+    numb: 3,
+    question: "when you write the domain Which page do you open first??",
+    answer: "index",
+    options: [
+      "home",
+      "profile",
+      "index",
+      "contact-us"
+    ]
+  },
+    {
+    numb: 4,
+    question: "google page",
+    answer: "Dynamic",
+    options: [
+      "None",
+      "Dynamic & Static",
+      "Static",
+      "Dynamic"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does HTTP stand for?",
+    answer: "Hyper Text Transfer Protocol",
+    options: [
+      "Hyper Text Transfer Prototype",
+      "Hyper Text Transfer Protocol",
+      "Hyper Text Transfer application",
+      "Hyper Text Transfer network"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
